2fdc0d490bd4e238a66955392c4cf6f11f1dc9f6
Nancy <nanc0603@stud.kea.dk>
2022-09-01 10:05:52 +0200

    fix the error

--- countarray.js
+++ countarray.js
@@ -1,25 +1,23 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", arrayCount);
 const givenArray = [];
-let counter = 0;
+let counter = -1;
 function arrayCount() {
-  console.log(arrayCount);
+  // console.log(arrayCount);
   counter++;
 
   delay();
 }
 function delay() {
-  console.log(delay);
-  if (counter <= 9) {
+  // console.log(delay);
+  if (counter < 100) {
     // show counting
-    console.log(counter);
-    givenArray.splice(0, 0, counter);
+    // console.log(counter);
+    givenArray.unshift(counter);
     console.log(givenArray);
-    setTimeout(arrayCount, 1000);
-  } else {
-    stop();
   }
-}
-function stop() {
-  console.log("counting goes on");
+  if (givenArray.length > 8) {
+    givenArray.pop();
+  }
+  setTimeout(arrayCount, 1000);
 }
